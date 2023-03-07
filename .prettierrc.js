8b881a507deefa66f1e9a8e87f08e158fe7df1dc
yetone <yetoneful@gmail.com>
2023-03-07 23:54:45 +0800

    chores: change prettierrc file name

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    singleQuote: true,
+    jsxSingleQuote: true,
+    semi: false,
+    trailingComma: 'es5',
+    tabWidth: 4,
+    useTabs: false,
+    quoteProps: 'consistent',
+    parser: 'typescript',
+    bracketSpacing: true,
+    printWidth: 120,
+}
