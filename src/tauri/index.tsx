c60e20f99e053f2df0005a1ba1e5233ef37ceace
yetone <yetoneful@gmail.com>
2023-03-09 18:20:56 +0800

    feat: create Tauri app (#125)
    * feat: create tauri app!!!
    
    * feat(ci): support release tauri apps

---
+++ src/tauri/index.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { App } from './App'
+
+const root = createRoot(document.getElementById('root')!)
+
+root.render(<App />)
