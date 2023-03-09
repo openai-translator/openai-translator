7036ad97cf1dd922b17b9a86f9cfd11148146b67
maltoze <maltoze@qq.com>
2023-03-09 13:06:49 +0800

    feat: add tts support to firefox (#104)
    * feat: add tts support to firefox
    
    * fix: make chrome tts support other languages; close #109

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,9 +1,32 @@
 import browser from 'webextension-polyfill'
+import { detectLang } from '../content_script/lang'
 
-browser.runtime.onMessage.addListener(function (request) {
-    if (typeof chrome !== 'undefined') {
+const isFirefox = /firefox/i.test(navigator.userAgent)
+
+async function handleSpeakDone() {
+    const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true })
+    tab.id && browser.tabs.sendMessage(tab.id, { type: 'speakDone' })
+}
+
+if (isFirefox) {
+    const utterance = new SpeechSynthesisUtterance()
+    utterance.addEventListener('end', handleSpeakDone)
+    browser.runtime.onMessage.addListener(async function (request) {
         if (request.type === 'speak') {
+            utterance.text = request.text
+            const lang = await detectLang(request.text)
+            utterance.lang = lang ?? 'en'
+            speechSynthesis.speak(utterance)
+        } else if (request.type === 'stopSpeaking') {
+            speechSynthesis.cancel()
+        }
+    })
+} else {
+    browser.runtime.onMessage.addListener(async function (request) {
+        if (request.type === 'speak') {
+            const lang = await detectLang(request.text)
             chrome.tts.speak(request.text, {
+                lang: lang ?? 'en',
                 onEvent: function (event) {
                     if (
                         event.type === 'end' ||
@@ -11,12 +34,14 @@ browser.runtime.onMessage.addListener(function (request) {
                         event.type === 'cancelled' ||
                         event.type === 'error'
                     ) {
-                        browser.runtime.sendMessage({ type: 'speakDone' })
+                        // TODO: interrupted event will cause error when PopupCard unmount
+                        // Uncaught (in promise) Error: Could not establish connection. Receiving end does not exist.
+                        handleSpeakDone()
                     }
                 },
             })
         } else if (request.type === 'stopSpeaking') {
             chrome.tts.stop()
         }
-    }
-})
+    })
+}
