173e1a5154637a1c4b1340d268566500d3a17fc3
yetone <yetoneful@gmail.com>
2023-03-09 23:56:35 +0800

    f

--- src/tauri/App.tsx
+++ src/tauri/App.tsx
@@ -1,13 +1,41 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { PopupCard } from '../content_script/PopupCard'
 import { Client as Styletron } from 'styletron-engine-atomic'
+import { appWindow } from '@tauri-apps/api/window'
 
 const engine = new Styletron({
     prefix: '__yetone-openai-translator-styletron-',
 })
 
 export function App() {
-    const isWindows = navigator.userAgent.indexOf('Windows') !== -1
+    const isMacOS = navigator.userAgent.includes('Mac OS X')
+    const isWindows = navigator.userAgent.includes('Windows')
+    const minimizeIconRef = useRef<HTMLDivElement>(null)
+    const maximizeIconRef = useRef<HTMLDivElement>(null)
+    const closeIconRef = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+        if (isMacOS) {
+            return
+        }
+        function handleMinimize() {
+            appWindow.minimize()
+        }
+        function handleMaximize() {
+            appWindow.maximize()
+        }
+        function handleClose() {
+            appWindow.close()
+        }
+        minimizeIconRef.current?.addEventListener('click', handleMinimize)
+        maximizeIconRef.current?.addEventListener('click', handleMaximize)
+        closeIconRef.current?.addEventListener('click', handleClose)
+        return () => {
+            minimizeIconRef.current?.removeEventListener('click', handleMinimize)
+            maximizeIconRef.current?.removeEventListener('click', handleMaximize)
+            closeIconRef.current?.removeEventListener('click', handleClose)
+        }
+    }, [])
 
     return (
         <div
@@ -16,7 +44,21 @@ export function App() {
                 height: '100%',
             }}
         >
-            <div className='draggable' data-tauri-drag-region />
+            <div className='titlebar' data-tauri-drag-region>
+                {!isMacOS && (
+                    <>
+                        <div className='titlebar-button' id='titlebar-minimize' ref={minimizeIconRef}>
+                            <img src='https://api.iconify.design/mdi:window-minimize.svg' alt='minimize' />
+                        </div>
+                        <div className='titlebar-button' id='titlebar-maximize' ref={maximizeIconRef}>
+                            <img src='https://api.iconify.design/mdi:window-maximize.svg' alt='maximize' />
+                        </div>
+                        <div className='titlebar-button' id='titlebar-close' ref={closeIconRef}>
+                            <img src='https://api.iconify.design/mdi:close.svg' alt='close' />
+                        </div>
+                    </>
+                )}
+            </div>
             <PopupCard
                 text=''
                 engine={engine}
