5db0869a744b183c6eb35e0b85af68e3cbb94ce2
maltoze <maltoze@qq.com>
2023-03-12 17:46:14 +0800

    fix(browser): move fetch to background

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,4 +1,5 @@
 import browser from 'webextension-polyfill'
+import { createParser } from 'eventsource-parser'
 
 const isFirefox = /firefox/i.test(navigator.userAgent)
 
@@ -62,3 +63,65 @@ if (isFirefox) {
         }
     })
 }
+
+browser.runtime.onConnect.addListener(async function (port) {
+    if (port.name !== 'background-fetch') {
+        return
+    }
+
+    const controller = new AbortController()
+    const { signal } = controller
+
+    port.onMessage.addListener(async function (message) {
+        if (message.type === 'abort') {
+            controller.abort()
+            return
+        }
+        if (message.type === 'open' && message?.details) {
+            const { url, options } = message.details
+
+            let response: Response
+            try {
+                response = await fetch(url, { ...options, signal })
+            } catch (error) {
+                if (error instanceof Error) {
+                    port.postMessage({
+                        error: { message: error.toString() },
+                    })
+                }
+                return
+            }
+
+            const reader = response?.body?.getReader()
+            if (!reader) {
+                port.postMessage({
+                    status: response.status,
+                })
+                return
+            }
+
+            const parser = createParser((event) => {
+                if (event.type === 'event') {
+                    port.postMessage({
+                        status: response.status,
+                        response: event.data,
+                    })
+                }
+            })
+
+            try {
+                // eslint-disable-next-line no-constant-condition
+                while (true) {
+                    const { done, value } = await reader.read()
+                    if (done) {
+                        break
+                    }
+                    const str = new TextDecoder().decode(value)
+                    parser.feed(str)
+                }
+            } finally {
+                reader.releaseLock()
+            }
+        }
+    })
+})
--- src/content_script/utils.ts
+++ src/content_script/utils.ts
@@ -1,7 +1,8 @@
 import { createParser } from 'eventsource-parser'
 import { userscriptFetch } from '../common/userscript-polyfill'
-import { isUserscript } from '../common/utils'
+import { isDesktopApp, isUserscript } from '../common/utils'
 import { containerTagName, popupCardID, popupThumbID, zIndex } from './consts'
+import browser from 'webextension-polyfill'
 
 function attachEventsToContainer($container: HTMLElement) {
     $container.addEventListener('mousedown', (event) => {
@@ -78,22 +79,51 @@ interface FetchSSEOptions extends RequestInit {
     onError(error: any): void
 }
 
+function handleBackgroundFetch(input: string, options: FetchSSEOptions) {
+    return new Promise((resolve) => {
+        const { onMessage, onError, signal, ...fetchOptions } = options
+
+        const port = browser.runtime.connect({ name: 'background-fetch' })
+        port.postMessage({ type: 'open', details: { url: input, options: fetchOptions } })
+        port.onMessage.addListener(function (message) {
+            if (message.error) {
+                onError(message)
+                resolve('')
+                return
+            }
+            if (message.status !== 200) {
+                onError(message)
+                return
+            }
+            onMessage(message.response)
+        })
+        signal?.addEventListener('abort', function () {
+            port.postMessage({ type: 'abort' })
+        })
+    })
+}
+
 export async function fetchSSE(input: string, options: FetchSSEOptions) {
     const { onMessage, onError, ...fetchOptions } = options
-    const fetch = isUserscript() ? userscriptFetch : window.fetch
-    const resp = await fetch(input, fetchOptions)
-    if (resp.status !== 200) {
-        onError(await resp.json())
-        return
-    }
-    const parser = createParser((event) => {
-        if (event.type === 'event') {
-            onMessage(event.data)
+
+    if (!isDesktopApp() && !isUserscript()) {
+        await handleBackgroundFetch(input, options)
+    } else {
+        const fetch = isUserscript() ? userscriptFetch : window.fetch
+        const resp = await fetch(input, fetchOptions)
+        if (resp.status !== 200) {
+            onError(await resp.json())
+            return
         }
-    })
-    for await (const chunk of streamAsyncIterator[Symbol.asyncIterator](resp.body)) {
-        const str = new TextDecoder().decode(chunk)
-        parser.feed(str)
+        const parser = createParser((event) => {
+            if (event.type === 'event') {
+                onMessage(event.data)
+            }
+        })
+        for await (const chunk of streamAsyncIterator[Symbol.asyncIterator](resp.body)) {
+            const str = new TextDecoder().decode(chunk)
+            parser.feed(str)
+        }
     }
 }
 
