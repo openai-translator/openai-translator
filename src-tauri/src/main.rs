cd19a21104da5e62adb546d247db87f4c69b4364
yetone <yetoneful@gmail.com>
2023-03-12 16:23:13 +0800

    fix: get mouse position

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -14,19 +14,48 @@ use crate::windows::{show_main_window, MAIN_WIN_NAME, show_main_window_with_sele
 use once_cell::sync::OnceCell;
 use tauri::AppHandle;
 use tauri::Manager;
+use tauri::api::notification::Notification;
 use window_shadows::set_shadow;
 
 pub static APP_HANDLE: OnceCell<AppHandle> = OnceCell::new();
 
+#[cfg(target_os = "macos")]
+fn query_accessibility_permissions() -> bool {
+    use macos_accessibility_client;
+    let trusted = macos_accessibility_client::accessibility::application_is_trusted_with_prompt();
+    if trusted {
+        print!("Application is totally trusted!");
+    } else {
+        print!("Application isn't trusted :(");
+    }
+    return trusted
+}
+
+#[cfg(not(target_os = "macos"))]
+fn query_accessibility_permissions() -> bool {
+    return true
+}
+
 fn main() {
     tauri::Builder::default()
         .setup(|app| {
+            let app_handle = app.handle();
             APP_HANDLE.get_or_init(|| app.handle());
             if cfg!(target_os = "windows") || cfg!(target_os = "linux") {
                 let window = app.get_window(MAIN_WIN_NAME).unwrap();
                 window.set_decorations(false)?;
                 // Try set shadow and ignore errors if it failed.
                 set_shadow(&window, true).unwrap_or_default();
+            }
+            if !query_accessibility_permissions() {
+                let window = app.get_window(MAIN_WIN_NAME).unwrap();
+                window.minimize().unwrap();
+                Notification::new(&app.config().tauri.bundle.identifier)
+                    .title("Accessibility permissions")
+                    .body("Please grant accessibility permissions to the app")
+                    .icon("icon.png")
+                    .notify(&app_handle)
+                    .unwrap();
             }
             Ok(())
         })
