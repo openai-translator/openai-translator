06ddf7166fb1b21b7ab99cc0d9e39734768d468d
yetone <yetoneful@gmail.com>
2023-03-16 01:00:42 +0800

    fix: eslint

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { TranslateMode, Provider  } from '../content_script/translate'
+import { TranslateMode, Provider } from '../content_script/translate'
 import { IBrowser, ThemeType } from './types'
 
 export interface ISettings {
