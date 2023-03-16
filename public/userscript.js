8a958c0909bf447798bb2a949feca1862df8ab73
maltoze <maltoze@qq.com>
2023-03-16 23:37:49 +0800

    fix(userscript): put css file into codebase and change default version

--- public/userscript.js
+++ public/userscript.js
@@ -5,11 +5,11 @@
 // @description     基于 ChatGPT API 的划词翻译浏览器插件和跨平台桌面端应用
 // @description:en  Browser extension and cross-platform desktop application for translation based on ChatGPT API
 // @author          yetone,maltoze
-// @icon64          https://cdn.jsdelivr.net/gh/yetone/openai-translator@main/public/icon.png
-// @icon            https://cdn.jsdelivr.net/gh/yetone/openai-translator@main/public/icon.png
+// @icon64          https://cdn.jsdelivr.net/gh/yetone/openai-translator@v0.1.0/public/icon.png
+// @icon            https://cdn.jsdelivr.net/gh/yetone/openai-translator@v0.1.0/public/icon.png
 // @match           *://*/*
-// @require         https://cdn.jsdelivr.net/gh/yetone/openai-translator@main/dist/userscript/index.js
-// @resource        MAIN_CSS https://cdn.jsdelivr.net/gh/yetone/openai-translator@main/dist/userscript/index.css
+// @require         https://cdn.jsdelivr.net/gh/yetone/openai-translator@v0.1.0/dist/userscript/index.js
+// @resource        MAIN_CSS https://cdn.jsdelivr.net/gh/yetone/openai-translator@v0.1.0/dist/userscript/index.css
 // @grant           GM.setValue
 // @grant           GM.getValue
 // @grant           GM_xmlhttpRequest
@@ -18,6 +18,7 @@
 // @license         MIT
 // ==/UserScript==
 
+/* eslint-disable no-undef */
 ;(function () {
     'use strict'
     const css = GM_getResourceText('MAIN_CSS')
