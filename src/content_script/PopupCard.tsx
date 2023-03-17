c753f8e34659e40b8a8f0638d8cc85c1e0a8c1f3
yuanx <yuanx.me@qq.com>
2023-03-17 21:19:41 +0800

    feat: The popup card follows pointer when scrolling

--- src/content_script/PopupCard.tsx
+++ src/content_script/PopupCard.tsx
@@ -304,6 +304,11 @@ export interface IPopupCardProps {
     onSettingsSave?: (oldSettings: ISettings) => void
 }
 
+export interface MovementXY {
+    x: number
+    y: number
+}
+
 export function PopupCard(props: IPopupCardProps) {
     const editorRef = useRef<HTMLTextAreaElement>(null)
     const isCompositing = useRef(false)
@@ -457,6 +462,8 @@ export function PopupCard(props: IPopupCardProps) {
 
     const actionButtonsRef = useRef<HTMLDivElement>(null)
 
+    const scrollYRef = useRef<number>(0)
+
     // Reposition the popup card to prevent it from extending beyond the screen.
     useEffect(() => {
         const calculateTranslatedContentMaxHeight = (): number => {
@@ -537,28 +544,46 @@ export function PopupCard(props: IPopupCardProps) {
             }
             e = e || window.event
             e.preventDefault()
-            const [l, t] = overflowCheck($popupCard, e)
+            const { movementX, movementY } = e
+            const [l, t] = overflowCheck($popupCard, { x: movementX, y: movementY })
             $popupCard.style.top = `${t}px`
             $popupCard.style.left = `${l}px`
         }
 
-        const overflowCheck = ($popupCard: HTMLDivElement, e: MouseEvent) => {
+        const overflowCheck = ($popupCard: HTMLDivElement, movementXY: MovementXY): number[] => {
             let left = $popupCard.offsetLeft
             let top = $popupCard.offsetTop
+            const { x: movementX, y: movementY } = movementXY
             if (
-                $popupCard.offsetLeft + e.movementX > documentPadding &&
-                window.innerWidth - $popupCard.offsetLeft - e.movementX - $popupCard.offsetWidth > documentPadding
+                $popupCard.offsetLeft + movementX > documentPadding &&
+                window.innerWidth - $popupCard.offsetLeft - movementX - $popupCard.offsetWidth > documentPadding
             ) {
-                left = $popupCard.offsetLeft + e.movementX
+                left = $popupCard.offsetLeft + movementX
             }
             if (
-                $popupCard.offsetTop + e.movementY > documentPadding &&
-                document.documentElement.offsetHeight - $popupCard.offsetTop - e.movementY - $popupCard.offsetHeight >
+                $popupCard.offsetTop + movementY > documentPadding &&
+                document.documentElement.offsetHeight - $popupCard.offsetTop - movementY - $popupCard.offsetHeight >
                     documentPadding
             ) {
-                top = $popupCard.offsetTop + e.movementY
+                top = $popupCard.offsetTop + movementY
             }
             return [left, top]
+        }
+
+        const elementScroll = async (e: globalThis.Event) => {
+            e.stopPropagation()
+            if (closed || !$popupCard) {
+                scrollYRef.current = window.scrollY
+                return
+            }
+            e = e || window.event
+            e.preventDefault()
+            const { scrollY } = window
+            const movementY = scrollY - scrollYRef.current
+            const [l, t] = overflowCheck($popupCard, { x: 0, y: movementY })
+            $popupCard.style.top = `${t}px`
+            $popupCard.style.left = `${l}px`
+            scrollYRef.current = scrollY
         }
 
         const closeDragElement = () => {
@@ -570,10 +595,12 @@ export function PopupCard(props: IPopupCardProps) {
 
         $header.addEventListener('mousedown', dragMouseDown)
         $header.addEventListener('mouseup', closeDragElement)
+        document.addEventListener('scroll', elementScroll)
 
         return () => {
             $header.removeEventListener('mousedown', dragMouseDown)
             $header.removeEventListener('mouseup', closeDragElement)
+            document.removeEventListener('scroll', elementScroll)
             closeDragElement()
         }
     }, [headerRef])
