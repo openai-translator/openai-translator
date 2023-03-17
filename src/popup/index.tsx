5087704652ef248074cb2fb7ce43e23a7066b52c
yetone <yetoneful@gmail.com>
2023-03-17 14:38:51 +0800

    fix: popup dark mode

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { PopupCard } from '../content_script/PopupCard'
 import { Client as Styletron } from 'styletron-engine-atomic'
 import '../i18n.js'
+import './index.css'
 
 const engine = new Styletron({
     prefix: '__yetone-openai-translator-styletron-',
@@ -13,6 +14,7 @@ const root = createRoot(document.getElementById('root') as HTMLElement)
 root.render(
     <React.StrictMode>
         <div
+            className='popup'
             style={{
                 position: 'relative',
                 minHeight: '100vh',
