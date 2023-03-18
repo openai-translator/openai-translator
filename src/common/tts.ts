a74b315379faf91f206fb28d6275be8d3ce9e19e
maltoze <maltoze@qq.com>
2023-03-18 22:51:33 +0800

    fix: mapping language tag and specify voice (#364)
    * fix: mapping lanuage tag
    
    * fix: specify voice
    
    * refactor

--- src/common/tts.ts
+++ src/common/tts.ts
@@ -4,12 +4,46 @@ interface SpeakOptions {
     onFinish?: () => void
 }
 
+const langMap: Record<string, string> = {
+    'en': 'en-US',
+    'zh-Hans': 'zh-CN',
+    'zh-Hant': 'zh-TW',
+    'yue': 'zh-HK',
+    'wyw': 'zh-CN',
+    'ja': 'ja-JP',
+    'ko': 'ko-KR',
+    'fr': 'fr-FR',
+    'de': 'de-DE',
+    'es': 'es-ES',
+    'it': 'it-IT',
+    'ru': 'ru-RU',
+    'pt': 'pt-PT',
+    'nl': 'nl-NL',
+    'pl': 'pl-PL',
+    'ar': 'ar-001',
+    'bg': 'bg-BG',
+    'ca': 'ca-ES',
+    'cs': 'cs-CZ',
+    'da': 'da-DK',
+    'el': 'el-GR',
+    'fi': 'fi-FI',
+    'he': 'he-IL',
+    'hi': 'hi-IN',
+    'hr': 'hr-HR',
+    'id': 'id-ID',
+    'vi': 'vi-VN',
+}
+
+const supportedVoices = speechSynthesis.getVoices()
+
 export function speak({ text, lang, onFinish }: SpeakOptions) {
     const utterance = new SpeechSynthesisUtterance()
     if (onFinish) {
         utterance.addEventListener('end', onFinish, { once: true })
     }
+    const langTag = langMap[lang ?? 'en'] ?? 'en-US'
     utterance.text = text
-    utterance.lang = lang ?? 'en'
+    utterance.lang = langTag
+    utterance.voice = supportedVoices.find((v) => v.lang === langTag) ?? null
     speechSynthesis.speak(utterance)
 }
