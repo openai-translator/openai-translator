740d572e5aadc45455ed525bb196b9c56c0ebb1a
maltoze <maltoze@qq.com>
2023-03-18 09:49:28 +0800

    fix: mapping lanuage tag

--- src/common/tts.ts
+++ src/common/tts.ts
@@ -4,12 +4,41 @@ interface SpeakOptions {
     onFinish?: () => void
 }
 
+const langMap: Record<string, string> = {
+    'en': 'en-US',
+    'zh-Hans': 'zh-CN',
+    'zh-Hant': 'zh-TW',
+    'yue': 'zh-HK',
+    'jp': 'jp-JP',
+    'ko': 'ko-KR',
+    'fr': 'fr-FR',
+    'de': 'de-DE',
+    'es': 'es-ES',
+    'it': 'it-IT',
+    'ru': 'ru-RU',
+    'pt': 'pt-PT',
+    'nl': 'nl-NL',
+    'pl': 'pl-PL',
+    'ar': 'ar-001',
+    'bg': 'bg-BG',
+    'ca': 'ca-ES',
+    'cs': 'cs-CZ',
+    'da': 'da-DK',
+    'el': 'el-GR',
+    'fi': 'fi-FI',
+    'he': 'he-IL',
+    'hi': 'hi-IN',
+    'hr': 'hr-HR',
+    'id': 'id-ID',
+    'vi': 'vi-VN',
+}
+
 export function speak({ text, lang, onFinish }: SpeakOptions) {
     const utterance = new SpeechSynthesisUtterance()
     if (onFinish) {
         utterance.addEventListener('end', onFinish, { once: true })
     }
     utterance.text = text
-    utterance.lang = lang ?? 'en'
+    utterance.lang = langMap[lang ?? 'en'] ?? 'en-US'
     speechSynthesis.speak(utterance)
 }
