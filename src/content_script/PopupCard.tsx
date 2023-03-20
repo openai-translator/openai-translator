cca1b657c7d9a1c825df50ab48aa5760fb7e9e80
安忆 <i@anyi.in>
2023-03-20 15:47:07 +0800

    feat: improve paragraph style (#392)
    * feat: reduce paragraph spacing
    
    https://github.com/yetone/openai-translator/issues/351
    
    * feat: make the translation results selectable
    
    * fix: lint

--- src/content_script/PopupCard.tsx
+++ src/content_script/PopupCard.tsx
@@ -138,7 +138,10 @@ const useStyles = createUseStyles({
         cursor: 'unset',
     }),
     'paragraph': {
-        margin: '14px 0',
+        'margin': '0.5em 0',
+        '-ms-user-select': 'text',
+        '-webkit-user-select': 'text',
+        'user-select': 'text',
     },
     'popupCardHeaderButtonGroup': {
         display: 'flex',
