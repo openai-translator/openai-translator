34d5140abd8d43f7bdeeabe89d72d655a83d2efb
猫猫 <overflowcat@gmail.com>
2023-03-22 22:57:13 +0800

    feat: i18n for `IpLocationNotification`

--- src/components/IpLocationNotification.tsx
+++ src/components/IpLocationNotification.tsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react'
+import { Trans, useTranslation } from 'react-i18next'
 import { Notification, KIND as NOTIFICATION_KIND } from 'baseui-sd/notification'
 import { StyledLink } from 'baseui-sd/link'
 import { IpLocation, getIpLocationInfo } from '../common/geo'
 
 export default function IpLocationNotification() {
+    const { t } = useTranslation()
+
     const [ipLocation, setIpLocation] = useState<IpLocation | null>(null)
     useEffect(() => {
         ;(async () => {
@@ -13,11 +16,7 @@ export default function IpLocationNotification() {
 
     if (ipLocation === null || ipLocation.supported) return <></>
 
-    const referenceLink = (
-        <StyledLink target='_blank' href='https://platform.openai.com/docs/supported-countries'>
-            supported region
-        </StyledLink>
-    )
+    const referenceLink = <StyledLink target='_blank' href='https://platform.openai.com/docs/supported-countries' />
 
     return ipLocation.name ? (
         <Notification
@@ -27,9 +26,13 @@ export default function IpLocationNotification() {
                 Body: { style: { width: 'auto' } },
             }}
         >
-            Your IP address is from {ipLocation.name}, which is not in a {referenceLink} of OpenAI. Continuing to use
-            without extra network configuration may result in your account being blocked by OpenAI, regardless of your
-            GPT Plus subscription status or any remaining account balance.
+            <Trans
+                i18nKey='Country Not Supported'
+                values={{
+                    name: ipLocation.name,
+                }}
+                components={[referenceLink]}
+            />
         </Notification>
     ) : (
         <Notification
@@ -39,9 +42,7 @@ export default function IpLocationNotification() {
                 Body: { style: { width: 'auto' } },
             }}
         >
-            We were unable to check if your IP address is in a {referenceLink} of OpenAI. Please ensure that you are
-            accessing the API in a supported location, or your account may get banned regardless of your GPT Plus
-            subscription status or any remaining account balance.
+            <Trans i18nKey='Country Not Detected' components={[referenceLink]} />
         </Notification>
     )
 }
