07f06ed5c43c5774c34d8bd80412e6ce1ebbc29f
ᡥᠠᡳᡤᡳᠶᠠ ᡥᠠᠯᠠ·ᠨᡝᡴᠣ 猫 <overflowcat@gmail.com>
2023-03-24 00:28:01 +0800

    feat: check geo info via OpenAI & i18n (#442)
    * feat: show notification when IP not supported
    
    * fix: `typos` spell check for country codes
    
    * feat: i18n for `IpLocationNotification`
    
    * feat: check geo info via OpenAI
    
    * optimize: check GeoPlugin first for speed
    
    * fix: remove GeoPlugin due to HTTP

---
+++ src/common/geo.ts
@@ -0,0 +1,57 @@
+import { userscriptFetch } from './userscript-polyfill'
+import { isUserscript } from './utils'
+import { ALLOWED_COUNTRY_CODES } from './geo-data' // a separate file for bypassing spell check
+
+export interface IpLocation {
+    supported: boolean
+    name?: string
+}
+
+interface OpenAICDNCGITraceResponse {
+    fl: string
+    h: string
+    ip: string
+    ts: string
+    visit_scheme: string
+    uag: string
+    colo: string
+    sliver: string
+    http: string
+    loc: string
+    tls: string
+    sni: string
+    warp: string
+    gateway: string
+    rbi: string
+    kex: string
+}
+
+function parseResponse(response: string): OpenAICDNCGITraceResponse {
+    console.log(response)
+    const params: Record<string, string> = {}
+    const pairs = response.split('\n')
+    for (const pair of pairs) {
+        const [key, value] = pair.split('=')
+        if (key && value) {
+            params[key.trim()] = value.trim()
+        }
+    }
+    return params as unknown as OpenAICDNCGITraceResponse
+}
+
+export async function getIpLocationInfo(): Promise<IpLocation> {
+    const fetch = isUserscript() ? userscriptFetch : window.fetch
+
+    const code = await fetch(
+        'https://chat.openai.com/cdn-cgi/trace', // API endpoint of OpenAI's CDN that returns location information. No authentication needed.
+        { cache: 'no-store' }
+    )
+        .then((response) => response.text() as string)
+        .then(parseResponse)
+        .then((o) => o.loc || '')
+        .catch(() => '')
+    return {
+        supported: ALLOWED_COUNTRY_CODES.has(code),
+        name: code,
+    }
+}
