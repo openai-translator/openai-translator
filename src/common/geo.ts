49de7f95d93bf1b08040ccc0f8bdfdd47c7ee378
猫猫 <overflowcat@gmail.com>
2023-03-23 21:51:03 +0800

    fix: remove GeoPlugin due to HTTP

--- src/common/geo.ts
+++ src/common/geo.ts
@@ -7,34 +7,26 @@ export interface IpLocation {
     name?: string
 }
 
-interface GeoPluginData {
-    geoplugin_request: string
-    geoplugin_status: number
-    geoplugin_delay: string
-    geoplugin_credit: string
-    geoplugin_city: string
-    geoplugin_region: string
-    geoplugin_regionCode: string
-    geoplugin_regionName: string
-    geoplugin_areaCode: string
-    geoplugin_dmaCode: string
-    geoplugin_countryCode: string
-    geoplugin_countryName: string
-    geoplugin_inEU: number
-    geoplugin_euVATrate: number
-    geoplugin_continentCode: string
-    geoplugin_continentName: string
-    geoplugin_latitude: string
-    geoplugin_longitude: string
-    geoplugin_locationAccuracyRadius: string
-    geoplugin_timezone: string
-    geoplugin_currencyCode: string
-    geoplugin_currencySymbol: string
-    geoplugin_currencySymbol_UTF8: string
-    geoplugin_currencyConverter: number
+interface OpenAICDNCGITraceResponse {
+    fl: string
+    h: string
+    ip: string
+    ts: string
+    visit_scheme: string
+    uag: string
+    colo: string
+    sliver: string
+    http: string
+    loc: string
+    tls: string
+    sni: string
+    warp: string
+    gateway: string
+    rbi: string
+    kex: string
 }
 
-function parseResponse(response: string): Record<string, string> {
+function parseResponse(response: string): OpenAICDNCGITraceResponse {
     console.log(response)
     const params: Record<string, string> = {}
     const pairs = response.split('\n')
@@ -44,32 +36,22 @@ function parseResponse(response: string): Record<string, string> {
             params[key.trim()] = value.trim()
         }
     }
-    return params
+    return params as unknown as OpenAICDNCGITraceResponse
 }
 
 export async function getIpLocationInfo(): Promise<IpLocation> {
     const fetch = isUserscript() ? userscriptFetch : window.fetch
 
-    // GeoPlugin is accessible in China, so try this first.
-    // Promise.All will take a long time to wait for timeout when OpenAI is blocked.
-    const [geoPluginCode, geoPluginName] = await fetch('http://www.geoplugin.net/json.gp', { cache: 'no-store' })
-        .then((response) => response.json() as GeoPluginData)
-        .then((data) => [data.geoplugin_countryCode, data.geoplugin_countryName])
-        .catch(() => ['', ''])
-    if (geoPluginCode !== '' && !ALLOWED_COUNTRY_CODES.has(geoPluginCode))
-        return { supported: false, name: geoPluginName }
-
-    // GeoPlugin's data is less accurate. Some unsupported IP addresses can only be detected by OpenAI.
-    const openAiCode = await fetch(
+    const code = await fetch(
         'https://chat.openai.com/cdn-cgi/trace', // API endpoint of OpenAI's CDN that returns location information. No authentication needed.
         { cache: 'no-store' }
     )
         .then((response) => response.text() as string)
         .then(parseResponse)
-        .then((o) => o['loc'] || '')
+        .then((o) => o.loc || '')
         .catch(() => '')
     return {
-        supported: ALLOWED_COUNTRY_CODES.has(openAiCode),
-        name: openAiCode,
+        supported: ALLOWED_COUNTRY_CODES.has(code),
+        name: code,
     }
 }
