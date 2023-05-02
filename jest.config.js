c45f021249ba3db3235a4bba3c8f7e4c01f6bde2
yetone <yetoneful@gmail.com>
2023-05-02 16:52:04 +0800

    feat: fix QuoteProcessor and add unit tests

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,10 @@
 module.exports = {
-    "roots": [
-        "src"
-    ],
-    "transform": {
-        "^.+\\.ts$": "ts-jest"
-    },
-}; 
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
