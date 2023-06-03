633a13506a1f3c2c9fe9cf4e315fb9131e862822
Yang Chao <iyeatse@gmail.com>
2023-06-03 17:58:57 +0800

    change bundle identifier

--- src-safari/Shared (App)/ViewController.swift
+++ src-safari/Shared (App)/ViewController.swift
@@ -16,7 +16,7 @@ import SafariServices
 typealias PlatformViewController = NSViewController
 #endif
 
-let extensionBundleIdentifier = "com.moecoder.OpenAI-Translator.Extension"
+let extensionBundleIdentifier = "xyz.yetone.OpenAI-Translator.Extension"
 
 class ViewController: PlatformViewController, WKNavigationDelegate, WKScriptMessageHandler {
 
