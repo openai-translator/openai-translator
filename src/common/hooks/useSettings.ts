a3629d007bd6154f06fcc5b681f7c501d27f23a9
maltoze <maltoze@qq.com>
2023-06-06 15:57:15 +0800

    fix: do not save settings

--- src/common/hooks/useSettings.ts
+++ src/common/hooks/useSettings.ts
@@ -1,17 +1,17 @@
 import useSWR from 'swr'
 
 import { ISettings } from '../types'
-import { getSettings, setSettings as saveSettings } from '../utils'
+import { getSettings } from '../utils'
 import { useCallback } from 'react'
 
 export function useSettings() {
     const { data: settings, mutate } = useSWR<ISettings>(['settings', getSettings], getSettings, { suspense: true })
 
     const setSettings = useCallback(
-        async (newSettings: ISettings) => {
-            await saveSettings(newSettings)
+        (newSettings: ISettings) => {
             mutate(newSettings, {
                 optimisticData: newSettings,
+                revalidate: false,
             })
         },
         [mutate]
