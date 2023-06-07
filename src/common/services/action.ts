3d5015138c26837562dbd000d6fd5d267c588f11
maltoze <maltoze@qq.com>
2023-06-07 18:22:58 +0800

    fix: support userscript (#831)

--- src/common/services/action.ts
+++ src/common/services/action.ts
@@ -1,5 +1,6 @@
 import { backgroundActionService } from '../background/services/action'
 import { IActionInternalService, actionInternalService } from '../internal-services/action'
-import { isDesktopApp } from '../utils'
+import { isDesktopApp, isUserscript } from '../utils'
 
-export const actionService: IActionInternalService = isDesktopApp() ? actionInternalService : backgroundActionService
+export const actionService: IActionInternalService =
+    isDesktopApp() || isUserscript() ? actionInternalService : backgroundActionService
---
+++ vite.config.userscript.ts
@@ -0,0 +1,50 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import monkey, { cdn } from 'vite-plugin-monkey'
+import { visualizer } from 'rollup-plugin-visualizer'
+import { version, license, name } from './package.json'
+
+const isDev = process.env.NODE_ENV === 'development'
+
+export default defineConfig({
+    plugins: [
+        tsconfigPaths(),
+        react(),
+        monkey({
+            entry: 'src/browser-extension/content_script/index.tsx',
+            userscript: {
+                name,
+                icon: 'https://cdn.jsdelivr.net/gh/openai-translator/openai-translator/public/icon.png',
+                namespace: 'https://github.com/openai-translator/openai-translator',
+                match: ['*://*/*'],
+                author: 'https://github.com/openai-translator',
+                license,
+                description: {
+                    '': '基于 ChatGPT API 的划词翻译浏览器插件和跨平台桌面端应用',
+                    'en': 'Browser extension and cross-platform desktop application for translation based on ChatGPT API',
+                },
+                require: [
+                    'https://cdn.jsdelivr.net/gh/openai-translator/openai-translator/public/cld-min.js',
+                    'data:application/javascript,%3Bwindow.detectLanguage%3DdetectLanguage%3B',
+                ],
+                version,
+            },
+            build: {
+                externalGlobals: {
+                    'react': cdn.jsdelivr('React', 'umd/react.production.min.js'),
+                    'react-dom': cdn.jsdelivr('ReactDOM', 'umd/react-dom.production.min.js'),
+                    'dexie': cdn.jsdelivr('Dexie', 'dist/dexie.min.js'),
+                },
+            },
+        }),
+        visualizer({ filename: 'dist/stats.html' }),
+    ],
+    build: {
+        assetsInlineLimit: 1024 * 1024, // 1mb
+        minify: !isDev,
+        sourcemap: isDev,
+        emptyOutDir: false,
+        rollupOptions: {},
+    },
+})
