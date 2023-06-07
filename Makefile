f73bd7f570e93ad65e745f17822342046fb5fc33
maltoze <maltoze@qq.com>
2023-06-07 14:06:19 +0800

    refactor: remove userscript bundle from codebase

--- Makefile
+++ Makefile
@@ -1,22 +1,21 @@
 VERSION ?= 0.1.0
 
-clean:
-	find dist/* -not -path "dist/userscript*" -prune -exec rm -rf {} \;
-
 change-version:
 	sed -i -e "s/\"version\": \".*\"/\"version\": \"$(VERSION)\"/" src/browser-extension/manifest.json
 	sed -i -e "s/\"version\": \".*\"/\"version\": \"$(VERSION)\"/" src/browser-extension/manifest.firefox.json
 	sed -i -e "s/\"version\": \".*\"/\"version\": \"$(VERSION)\"/" src-tauri/tauri.conf.json
-	sed -i -e "s/\"version\": \".*\"/\"version\": \"$(VERSION)\"/" package.json
-	sed -i -e "s/\/\/ @version.*/\/\/ @version $(VERSION)/" public/userscript.js
-	sed -i -E 's|openai-translator@[^/]+|openai-translator@v$(VERSION)|g' public/userscript.js
 
-build-browser-extension: change-version
-	pnpm vite build -c vite.config.userscript.ts
+change-package-version:
+	sed -i -e "s/\"version\": \".*\"/\"version\": \"$(VERSION)\"/" package.json
+
+build-browser-extension: change-version change-package-version
 	pnpm vite build -c vite.config.chromium.ts
 	pnpm vite build -c vite.config.firefox.ts
 	cd dist/browser-extension/chromium && zip -r ../chromium.zip .
 	cd dist/browser-extension/firefox && zip -r ../firefox.zip .
+
+build-userscript: change-package-version
+	pnpm vite build -c vite.config.userscript.ts
 
 build-popclip-extension:
 	rm -f dist/openai-translator.popclipextz
--- vite.config.userscript.ts
+++ vite.config.userscript.ts
@@ -1,21 +1,39 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import monkey from 'vite-plugin-monkey'
+import { visualizer } from 'rollup-plugin-visualizer'
+import { version, license, name } from './package.json'
 
 const isDev = process.env.NODE_ENV === 'development'
 
 export default defineConfig({
-    plugins: [tsconfigPaths(), react()],
+    plugins: [
+        tsconfigPaths(),
+        react(),
+        monkey({
+            entry: 'src/browser-extension/content_script/index.tsx',
+            userscript: {
+                name,
+                icon: 'https://cdn.jsdelivr.net/gh/openai-translator/openai-translator/public/icon.png',
+                namespace: 'https://github.com/openai-translator/openai-translator',
+                match: ['*://*/*'],
+                author: 'https://github.com/openai-translator',
+                license,
+                description: {
+                    '': '基于 ChatGPT API 的划词翻译浏览器插件和跨平台桌面端应用',
+                    'en': 'Browser extension and cross-platform desktop application for translation based on ChatGPT API',
+                },
+                require: ['https://cdn.jsdelivr.net/gh/openai-translator/openai-translator/public/cld-min.js'],
+                version,
+            },
+        }),
+        visualizer({ filename: 'dist/stats.html' }),
+    ],
     build: {
         assetsInlineLimit: 1024 * 1024, // 1mb
         minify: !isDev,
         sourcemap: isDev,
-        rollupOptions: {
-            input: 'src/browser-extension/content_script/index.tsx',
-            output: {
-                entryFileNames: 'userscript/[name].js',
-                format: 'iife',
-            },
-        },
+        rollupOptions: {},
     },
 })
