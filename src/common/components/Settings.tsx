bb4c9d952f4c155a04cc3e6c4f33b5ed59e91425
maltoze <maltoze@qq.com>
2023-06-14 17:32:57 +0800

    feat: support new models

--- src/common/components/Settings.tsx
+++ src/common/components/Settings.tsx
@@ -547,11 +547,16 @@ function APIModelSelector({ provider, value, onChange, onBlur }: APIModelSelecto
         if (provider === 'OpenAI') {
             setOptions([
                 { label: 'gpt-3.5-turbo', id: 'gpt-3.5-turbo' },
+                { label: 'gpt-3.5-turbo-0613', id: 'gpt-3.5-turbo-0613' },
                 { label: 'gpt-3.5-turbo-0301', id: 'gpt-3.5-turbo-0301' },
+                { label: 'gpt-3.5-turbo-16k', id: 'gpt-3.5-turbo-16k' },
+                { label: 'gpt-3.5-turbo-16k-0613', id: 'gpt-3.5-turbo-16k-0613' },
                 { label: 'gpt-4', id: 'gpt-4' },
                 { label: 'gpt-4-0314', id: 'gpt-4-0314' },
+                { label: 'gpt-4-0613', id: 'gpt-4-0613' },
                 { label: 'gpt-4-32k', id: 'gpt-4-32k' },
                 { label: 'gpt-4-32k-0314', id: 'gpt-4-32k-0314' },
+                { label: 'gpt-4-32k-0613', id: 'gpt-4-32k-0613' },
             ])
         } else if (provider === 'ChatGPT') {
             setIsLoading(true)
