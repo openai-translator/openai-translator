5806315a5f7fbbfd5f50f49d4501ecee2fac8954
maltoze <maltoze@qq.com>
2023-06-19 15:35:37 +0800

    fix: test can pass in headless mode

--- e2e/hotkey.spec.ts
+++ e2e/hotkey.spec.ts
@@ -4,26 +4,29 @@ import { expect, test } from './fixtures'
 import { containerID, popupCardID } from '../src/browser-extension/content_script/consts'
 
 test.fixme('hotkey should work', async ({ page, extensionId }) => {
-    await page.goto(getOptionsPageUrl(extensionId))
-    const input = page.locator('input[name="apiKey"]')
-    await input.fill('fake-api-key')
-    await page.getByTestId('hotkey-recorder').click()
-    await page.keyboard.down('Alt')
-    await page.keyboard.down('x')
-    await page.keyboard.up('Alt')
-    await page.keyboard.up('x')
-    await page.getByText('Save').click()
+    await test.step('set hotkey', async () => {
+        await page.goto(getOptionsPageUrl(extensionId))
+        const input = page.locator('input[name="apiKey"]')
+        await input.fill('fake-api-key')
+        await page.getByTestId('hotkey-recorder').click()
+        await page.keyboard.down('Alt')
+        await page.keyboard.down('x')
+        await page.keyboard.up('Alt')
+        await page.keyboard.up('x')
+        await page.getByText('Save').click()
+    })
 
-    await page.goto(`file:${path.join(__dirname, 'test.html')}`)
-    await selectExampleText(page)
+    await test.step('select example text', async () => {
+        await page.goto(`file:${path.join(__dirname, 'test.html')}`)
+        await selectExampleText(page)
 
-    await page.keyboard.down('Alt')
-    await page.keyboard.down('x')
-    await page.keyboard.up('Alt')
-    await page.keyboard.up('x')
+        const container = page.locator(`#${containerID}`)
+        await container.waitFor({ state: 'attached' })
 
-    const container = page.locator(`#${containerID}`)
-    await expect(container).toBeAttached()
-    const popupCard = container.locator(`#${popupCardID}`)
-    await expect(popupCard).toBeVisible()
+        await page.keyboard.down('Alt')
+        await page.keyboard.press('x')
+
+        const popupCard = container.locator(`#${popupCardID}`)
+        await expect(popupCard).toBeVisible()
+    })
 })
