4841df9c7aaae79a4a037e7fc92f2d6582c06dd5
maltoze <maltoze@qq.com>
2023-08-03 10:50:32 +0800

    fix: remove duplicate Toaster

--- src/common/components/CopyButton.tsx
+++ src/common/components/CopyButton.tsx
@@ -2,31 +2,28 @@ import { Tooltip } from 'baseui-sd/tooltip'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
 import { RxCopy } from 'react-icons/rx'
 import { useTranslation } from 'react-i18next'
-import toast, { Toaster } from 'react-hot-toast'
+import toast from 'react-hot-toast'
 
 export function CopyButton({ text, styles }: { text: string; styles: { actionButton: string } }) {
     const { t } = useTranslation()
     return (
-        <>
-            <Tooltip content={t('Copy to clipboard')} placement='bottom'>
-                <div>
-                    <CopyToClipboard
-                        text={text}
-                        onCopy={() => {
-                            toast(t('Copy to clipboard'), {
-                                duration: 3000,
-                                icon: '👏',
-                            })
-                        }}
-                        options={{ format: 'text/plain' }}
-                    >
-                        <div className={styles.actionButton}>
-                            <RxCopy size={13} />
-                        </div>
-                    </CopyToClipboard>
-                </div>
-            </Tooltip>
-            <Toaster />
-        </>
+        <Tooltip content={t('Copy to clipboard')} placement='bottom'>
+            <div>
+                <CopyToClipboard
+                    text={text}
+                    onCopy={() => {
+                        toast(t('Copy to clipboard'), {
+                            duration: 3000,
+                            icon: '👏',
+                        })
+                    }}
+                    options={{ format: 'text/plain' }}
+                >
+                    <div className={styles.actionButton}>
+                        <RxCopy size={13} />
+                    </div>
+                </CopyToClipboard>
+            </div>
+        </Tooltip>
     )
 }
