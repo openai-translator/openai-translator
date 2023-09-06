935512e67c3b99ad7d041ad1f711850a144a242a
yetone <yetoneful@gmail.com>
2023-09-07 01:29:34 +0800

    optimize: use enigo to write string

--- src-tauri/src/writing.rs
+++ src-tauri/src/writing.rs
@@ -1,5 +1,6 @@
-use arboard::ImageData;
 use parking_lot::Mutex;
+use std::{thread, time::Duration};
+use enigo::{Enigo, KeyboardControllable};
 
 #[allow(dead_code)]
 #[cfg(target_os = "windows")]
@@ -20,8 +21,6 @@ static SELECT_ALL: Mutex<()> = Mutex::new(());
 #[allow(dead_code)]
 #[cfg(target_os = "macos")]
 pub fn select_all() {
-    // use std::{thread, time::Duration};
-
     use enigo::*;
 
     let _guard = SELECT_ALL.lock();
@@ -36,9 +35,6 @@ pub fn select_all() {
     enigo.key_up(Key::Option);
     enigo.key_down(Key::Meta);
     enigo.key_click(Key::Layout('a'));
-    // enigo.key_down(Key::Layout('c'));
-    // thread::sleep(Duration::from_millis(300));
-    // enigo.key_up(Key::Layout('c'));
     enigo.key_up(Key::Meta);
 }
 
@@ -56,7 +52,10 @@ pub fn select_all() {
     enigo.key_up(Key::Control);
 }
 
+static INPUT_LOCK: Mutex<()> = Mutex::new(());
+
 pub fn double_backspace_click() {
+    let _guard = INPUT_LOCK.lock();
     use enigo::*;
     let mut enigo = Enigo::new();
     enigo.key_click(Key::Backspace);
@@ -64,13 +63,18 @@ pub fn double_backspace_click() {
 }
 
 pub fn double_left_click() {
+    let _guard = INPUT_LOCK.lock();
     use enigo::*;
     let mut enigo = Enigo::new();
+    thread::sleep(Duration::from_millis(100));
     enigo.key_click(Key::LeftArrow);
+    thread::sleep(Duration::from_millis(100));
     enigo.key_click(Key::LeftArrow);
+    thread::sleep(Duration::from_millis(100));
 }
 
 pub fn double_right_click() {
+    let _guard = INPUT_LOCK.lock();
     use enigo::*;
     let mut enigo = Enigo::new();
     enigo.key_click(Key::RightArrow);
@@ -80,6 +84,8 @@ pub fn double_right_click() {
 #[allow(dead_code)]
 #[cfg(target_os = "windows")]
 pub fn paste() {
+    let _guard = INPUT_LOCK.lock();
+    let __guard = PASTE.lock();
     use enigo::*;
     let mut enigo = Enigo::new();
     enigo.key_up(Key::Control);
@@ -96,12 +102,9 @@ static PASTE: Mutex<()> = Mutex::new(());
 #[allow(dead_code)]
 #[cfg(target_os = "macos")]
 pub fn paste() {
-    // use std::{thread, time::Duration};
-
+    let _guard = INPUT_LOCK.lock();
+    let __guard = PASTE.lock();
     use enigo::*;
-
-    let _guard = PASTE.lock();
-
     let mut enigo = Enigo::new();
     enigo.key_up(Key::Control);
     enigo.key_up(Key::Meta);
@@ -112,15 +115,14 @@ pub fn paste() {
     enigo.key_up(Key::Option);
     enigo.key_down(Key::Meta);
     enigo.key_click(Key::Layout('v'));
-    // enigo.key_down(Key::Layout('c'));
-    // thread::sleep(Duration::from_millis(300));
-    // enigo.key_up(Key::Layout('c'));
     enigo.key_up(Key::Meta);
 }
 
 #[allow(dead_code)]
 #[cfg(target_os = "linux")]
 pub fn paste() {
+    let _guard = INPUT_LOCK.lock();
+    let __guard = PASTE.lock();
     use enigo::*;
     let mut enigo = Enigo::new();
     enigo.key_up(Key::Control);
@@ -134,7 +136,6 @@ pub fn paste() {
 
 pub fn get_input_text() -> Result<String, Box<dyn std::error::Error>> {
     use arboard::Clipboard;
-    use std::{thread, time::Duration};
 
     let old_clipboard = (Clipboard::new()?.get_text(), Clipboard::new()?.get_image());
 
@@ -201,85 +202,58 @@ pub fn get_input_text() -> Result<String, Box<dyn std::error::Error>> {
 
 #[tauri::command]
 pub fn writing() {
-    use arboard::Clipboard;
     let content = get_input_text().unwrap_or_default();
-    let old_text = Clipboard::new().unwrap().get_text();
-    let old_image = Clipboard::new().unwrap().get_image();
-    {
-        let mut old_clipboard_content = OLD_CLIPBOARD_CONTENT.lock();
-        *old_clipboard_content = (old_text.ok(), old_image.ok());
-    }
-    do_write_to_input("Translating, please wait... ✍️".to_string());
-    select_all();
+    do_write_to_input("Translating, please wait... ✍️".to_string(), false);
     crate::utils::writing_text(content);
 }
 
 static START_WRITING: Mutex<bool> = Mutex::new(false);
-static OLD_CLIPBOARD_CONTENT: Mutex<(Option<String>, Option<ImageData<'static>>)> = Mutex::new((None, None));
 
-fn do_write_to_input(text: String) {
-    use arboard::Clipboard;
-    use std::{thread, time::Duration};
-    let mut write_clipboard = Clipboard::new().unwrap();
-
-    write_clipboard.set_text(text).unwrap();
-    thread::sleep(Duration::from_millis(50));
-    paste();
+fn do_write_to_input(text: String, animation: bool) {
+    let _guard = INPUT_LOCK.lock();
+    let mut enigo = Enigo::new();
+    if animation {
+        for c in text.chars() {
+            let char = c.to_string();
+            enigo.key_sequence(&char);
+            thread::sleep(Duration::from_millis(20));
+        }
+    } else {
+        enigo.key_sequence(&text);
+    }
 }
 
 #[tauri::command]
 pub fn write_to_input(text: String) {
-    // println!("write_to_input: {}", text);
-    use std::{thread, time::Duration};
     let mut new_text = text.clone();
     let mut start_writing = START_WRITING.lock();
     let is_first_writing = !*start_writing;
     if is_first_writing {
-        new_text = text.clone() + " ✍️";
+        select_all();
+        thread::sleep(Duration::from_millis(50));
+        // new_text = text.clone() + " ✍️";
     }
     *start_writing = true;
-    do_write_to_input(new_text);
-    if is_first_writing {
-        thread::sleep(Duration::from_millis(50));
-        double_left_click();
-    }
+    do_write_to_input(new_text, true);
+    // if is_first_writing {
+    //     double_left_click();
+    // }
 }
 
 #[tauri::command]
 pub fn finish_writing() {
-    use arboard::Clipboard;
-    use std::{thread, time::Duration};
-
     let mut start_writing = START_WRITING.lock();
     *start_writing = false;
 
-    double_right_click();
-    thread::sleep(Duration::from_millis(50));
+    // double_right_click();
+    // thread::sleep(Duration::from_millis(50));
+    //
+    // double_backspace_click();
+    // thread::sleep(Duration::from_millis(50));
 
-    double_backspace_click();
-    thread::sleep(Duration::from_millis(50));
-
-    do_write_to_input(" ✅".to_string());
+    do_write_to_input(" ✅".to_string(), true);
     thread::sleep(Duration::from_millis(300));
 
     double_backspace_click();
     thread::sleep(Duration::from_millis(50));
-
-    let old_clipboard_content = OLD_CLIPBOARD_CONTENT.lock().clone();
-    let (old_text, old_image) = old_clipboard_content;
-    let mut write_clipboard = Clipboard::new().unwrap();
-    match old_text {
-        Some(text) => {
-            write_clipboard.set_text(text.clone()).unwrap();
-        }
-        None => {
-            write_clipboard.clear().unwrap();
-        }
-    }
-    match old_image {
-        Some(image) => {
-            write_clipboard.set_image(image.clone()).unwrap();
-        }
-        None => {}
-    }
 }
