176ae9bc413b8701893563be382ebf58531329c5
yetone <yetoneful@gmail.com>
2023-09-07 16:16:41 +0800

    optimize: speed up select all (#986)

--- src-tauri/src/writing.rs
+++ src-tauri/src/writing.rs
@@ -142,15 +142,13 @@ pub fn get_input_text() -> Result<String, Box<dyn std::error::Error>> {
 
     write_clipboard.set_text(not_selected_placeholder)?;
 
-    thread::sleep(Duration::from_millis(50));
-
     select_all();
 
-    thread::sleep(Duration::from_millis(50));
+    thread::sleep(Duration::from_millis(10));
 
     crate::utils::copy();
 
-    thread::sleep(Duration::from_millis(100));
+    thread::sleep(Duration::from_millis(10));
 
     let new_text = Clipboard::new()?.get_text();
 
