da671d13dca4afcd97de380237308c1092f7ed01
yetone <yetoneful@gmail.com>
2024-03-24 15:28:10 +0800

    fix: Revert "optimize: refine prompts (#1358)" (#1385)
    * Revert "optimize: refine prompts (#1358)"
    
    This reverts commit 5f8730e59143ab144413cc18a3b42e49164ecd5a.
    
    * fix: translate deps

--- src/common/engines/claude.ts
+++ src/common/engines/claude.ts
@@ -51,24 +51,8 @@ export class Claude extends AbstractEngine {
                 },
                 {
                     role: 'assistant',
-                    content: `Yes, that's me.`,
+                    content: 'Ok, I will do that.',
                 },
-                ...(req.assistantPrompts?.reduce(
-                    (acc, cur) => {
-                        return [
-                            ...acc,
-                            {
-                                role: 'user',
-                                content: cur,
-                            },
-                            {
-                                role: 'assistant',
-                                content: 'I understand, I will strictly do as you said.',
-                            },
-                        ]
-                    },
-                    [] as { role: string; content: string }[]
-                ) ?? []),
                 {
                     role: 'user',
                     content: req.commandPrompt,
