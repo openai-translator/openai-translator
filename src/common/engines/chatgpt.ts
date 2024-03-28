00a887b35ac18b15481cf99a1c9504412ebc248b
BlackStar1453 <69788237+BlackStar1453@users.noreply.github.com>
2024-03-28 10:05:03 +0800

    Revert "Revert "delete api.js""
    This reverts commit 73f9899052eac4cfb31cb506b8b29c2add444583.

--- src/common/engines/chatgpt.ts
+++ src/common/engines/chatgpt.ts
@@ -6,7 +6,6 @@ import * as utils from '../utils'
 import { codeBlock } from 'common-tags'
 import { fetchSSE } from '../utils'
 import { AbstractEngine } from './abstract-engine'
-import { ArkoseToken } from '../arkose'
 import Browser from 'webextension-polyfill'
 
 
@@ -136,7 +135,6 @@ export class ChatGPT extends AbstractEngine {
         }
         const respJson = await resp?.json()
         const apiKey = respJson.accessToken
-        const initializeArkoseToken =  await ArkoseToken()
         const arkoseToken = await getArkoseToken()
         const requirements = await getChatRequirements(apiKey)
         const requirementstoken = requirements.token
